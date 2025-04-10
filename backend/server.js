370bec43902c03922ec9fa54880c4d29c0c50e97
Gedaias <gedaias@geda.dev>
2025-04-09 19:43:33 -0600

    Set up Express server and define root route

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 
+const app = express();
+
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
