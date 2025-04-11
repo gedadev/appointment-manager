c75603e093115bcce370898e6de2f3544dd0037f
Gedaias <gedaias@geda.dev>
2025-04-10 21:05:51 -0600

    Implement user routes for user creation and login functionality

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const User = require("./models/User");
-const bcrypt = require("bcryptjs");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
@@ -13,47 +12,7 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.post("/create", async (req, res) => {
-  const { name, email, password, businessName } = req.body;
-  const foundUser = await User.findOne({ email });
-  const foundBusiness = await User.findOne({ businessName });
-
-  if (foundUser) return res.status(400).send("User already registered");
-  if (foundBusiness) return res.status(400).send("Business already registered");
-
-  try {
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      passwordHash,
-      businessName,
-      businessSlug: businessName.toLowerCase().replace(/\s+/g, "-"),
-    });
-
-    res.status(201).send("User created successfully");
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const foundUser = await User.findOne({ email });
-
-  if (!foundUser) return res.status(400).send("Login Failed");
-
-  try {
-    const isMatch = await bcrypt.compare(password, foundUser.passwordHash);
-
-    if (!isMatch) return res.status(400).send("Login Failed");
-
-    res.send("Login success");
-  } catch {
-    res.status(500).send();
-  }
-});
+app.use("/user", userRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
