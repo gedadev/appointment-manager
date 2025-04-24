b21109c12e1709f729f49d76d97a10d4c5f8e5d1
Gedaias <gedaias@geda.dev>
2025-04-23 21:42:58 -0600

    Add custom hook for form validations, including email and password checks

---
+++ frontend/src/hooks/useFormValidations.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export const useFormValidations = () => {
+  const [formError, setFormError] = useState(null);
+
+  const validateEmail = (email) => {
+    const emailRegex = /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/;
+
+    if (!email) {
+      setFormError({ message: "Enter your email", input: "email" });
+      return false;
+    } else if (!emailRegex.test(email)) {
+      setFormError({ message: "Enter a valid email", input: "email" });
+      return false;
+    } else {
+      setFormError(null);
+      return true;
+    }
+  };
+
+  const validatePassword = (password) => {
+    const minLength = password.length >= 8;
+    const hasUpper = /[A-Z]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const noSpaces = !/\s/.test(password);
+
+    if (!password) {
+      setFormError({ message: "Enter your password", input: "password" });
+      return false;
+    } else if (!noSpaces) {
+      setFormError({
+        message: "Password can't contain spaces",
+        input: "password",
+      });
+      return false;
+    } else if (!hasUpper || !hasLower || !hasNumber) {
+      setFormError({
+        message:
+          "Password must contain a lower case letter, an upper case letter and a number",
+        input: "password",
+      });
+      return false;
+    } else if (!minLength) {
+      setFormError({
+        message: "Password must have at least 8 characters",
+        input: "password",
+      });
+      return false;
+    } else {
+      setFormError(null);
+      return true;
+    }
+  };
+
+  const validateForm = () => {
+    return false;
+  };
+
+  return {
+    validateForm,
+    validateEmail,
+    validatePassword,
+    formError,
+  };
+};
