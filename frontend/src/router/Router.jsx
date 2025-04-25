3182825d5691b54de619a0ef2e1fe9835d1ea3fc
Gedaias <gedaias@geda.dev>
2025-04-24 19:35:27 -0600

    Import Home component into Router; remove inline Home component definition for cleaner code organization

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import { Home } from "../pages/Home";
 
 const Dashboard = () => <div>Dashboard Page</div>;
-const Home = () => <div>Home Page</div>;
 
 const router = createBrowserRouter([
   {
