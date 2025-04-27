384b0b04c27b5cf507cbc1baee9a8e13be0ac086
Gedaias <gedaias@geda.dev>
2025-04-27 16:29:34 -0600

    Enhance Navbar component to display user greeting and dropdown on login; add user menu styles for improved UX

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export function Navbar() {
   const navigate = useNavigate();
+  const { userData, userIsLogged } = useAuth();
+
+  const getName = () => (userData ? userData.name.split(" ")[0] : null);
 
   return (
     <div className="navbar">
       <div className="navbar-branding">Brand</div>
-      <button className="login-button-nav" onClick={() => navigate("/login")}>
-        Login
-      </button>
+      {userIsLogged ? (
+        <div className="user-menu-nav">
+          <p>{`Welcome, ${getName()}`}</p>
+          <IoMdArrowDropdown />
+        </div>
+      ) : (
+        <button className="login-button-nav" onClick={() => navigate("/login")}>
+          Login
+        </button>
+      )}
     </div>
   );
 }
