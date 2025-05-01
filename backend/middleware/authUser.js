a337d5211927e3e909bb9ff791762b3a1f878cb7
Gedaias <gedaias@geda.dev>
2025-04-30 23:06:20 -0600

    Refactor authUser middleware to improve token verification and error handling

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -2,17 +2,24 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const authUser = async (req, res, next) => {
-  let token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).send("Unauthorized");
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
-    const data = jwt.verify(token, process.env.ACCESS_TOKEN);
+    const data = jwt.verify(token, process.env.ACCESS_TOKEN, (error, data) => {
+      if (error) throw new Error("Invalid token");
+      return data;
+    });
 
-    req.user = await User.findById(data.userId).select("-passwordHash");
+    const user = await User.findById(data.userId).select("-passwordHash");
+
+    if (!user) throw new Error();
+
+    req.user = user;
     next();
-  } catch {
-    res.status(403).send("Session expired");
+  } catch (error) {
+    res.status(403).json({ message: error.message || "Session expired" });
   }
 };
 
