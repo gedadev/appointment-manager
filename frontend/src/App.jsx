33b33ffba593b35456d5aeade507ec42a08aff0f
Gedaias <gedaias@geda.dev>
2025-06-09 23:03:33 -0600

    Add react-hot-toast for notifications and integrate it in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./styles/App.css";
 import { AuthProvider } from "./context/AuthProvider";
 import AppRouter from "./router/Router";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <AuthProvider>
+      <Toaster />
       <AppRouter />
     </AuthProvider>
   );
