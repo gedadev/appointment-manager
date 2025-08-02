67dad7812f199a52be7635c48c4517b61c6264bc
Gedaias <gedaias@geda.dev>
2025-08-01 21:23:18 -0600

    Add new appointment button and modal to Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,17 +5,24 @@ import { IoMdArrowDropdown } from "react-icons/io";
 import { MdDashboard } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { CiLogout } from "react-icons/ci";
+import { FiFilePlus } from "react-icons/fi";
+import { AppointmentModal } from "./AppointmentModal";
 
 export function Navbar() {
   const [activeMenu, setActiveMenu] = useState(false);
+  const [activeModal, setActiveModal] = useState(false);
   const navigate = useNavigate();
   const { userData, userIsLogged } = useAuth();
 
   const getName = () => (userData ? userData.name.split(" ")[0] : "pal");
 
+  const toggleModal = () => setActiveModal(!activeModal);
+
   return (
     <div className="navbar">
-      <div className="navbar-branding">Brand</div>
+      <div className="navbar-branding">
+        <Link to={"/"}>Brand</Link>
+      </div>
       {userIsLogged ? (
         <>
           <div
@@ -25,7 +32,11 @@ export function Navbar() {
             <p>{`Welcome, ${getName()}`}</p>
             <IoMdArrowDropdown />
           </div>
-          <UserMenu activeMenu={activeMenu} />
+          <UserMenu activeMenu={activeMenu} toggleModal={toggleModal} />
+          <AppointmentModal
+            activeModal={activeModal}
+            toggleModal={toggleModal}
+          />
         </>
       ) : (
         <button className="login-button-nav" onClick={() => navigate("/login")}>
@@ -36,7 +47,7 @@ export function Navbar() {
   );
 }
 
-function UserMenu({ activeMenu }) {
+function UserMenu({ activeMenu, toggleModal }) {
   const { logout } = useAuth();
 
   return (
@@ -49,6 +60,9 @@ function UserMenu({ activeMenu }) {
       }}
     >
       <ul>
+        <button className="new-appointment-button" onClick={toggleModal}>
+          New Appointment <FiFilePlus />
+        </button>
         <Link to={"/profile"}>
           <li>
             Profile <CgProfile />
