198ec156c66aedc4b718dfc8ec77b5d59180ba89
Gedaias <gedaias@geda.dev>
2025-08-19 20:22:35 -0600

    Implement appointment addition logic in AppointmentProvider

--- frontend/src/context/AppointmentProvider.jsx
+++ frontend/src/context/AppointmentProvider.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useApi } from "../hooks/useApi";
 import { AppointmentContext } from "./AppointmentContext";
 
 export const AppointmentProvider = ({ children }) => {
-  const value = {};
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { endpoints, request } = useApi();
+
+  const addAppointment = async (data) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await request(endpoints.appointment.new, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      if (response instanceof Error) throw response;
+
+      return { success: true };
+    } catch (error) {
+      setError(error.message);
+      return { success: false };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    loading,
+    error,
+    addAppointment,
+  };
 
   return (
     <AppointmentContext.Provider value={value}>
