e2ca836f16a8045c9cb7f129f4834184725ab6bc
Gedaias <gedaias@geda.dev>
2025-08-27 18:22:09 -0600

    Add AppointmentCard component to display appointment details

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import "../styles/Dashboard.css";
+import { useState } from "react";
 import { Header } from "../components/Dashboard/Header";
 import { useAppointment } from "../hooks/useAppointment";
 
@@ -9,6 +10,21 @@ export function Dashboard() {
     <main className="dashboard">
       <section className="appointment-section">
         <Header />
+        <div className="summary-section">
+          <div className="summary-container"></div>
+          <div className="next-appointments-container">
+            {appointments.length > 0 &&
+              appointments.map((appointment) => (
+                <div className="appointment-card" key={appointment._id}>
+                  <p>{appointment.customerName}</p>
+                  <p>{appointment.date}</p>
+                  <p>{appointment.time}</p>
+                  <p>{appointment.cost}</p>
+                  <p>{appointment.notes}</p>
+                </div>
+              ))}
+          </div>
+        </div>
       </section>
     </main>
   );
