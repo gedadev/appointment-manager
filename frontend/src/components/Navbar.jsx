8cfd1c51893155d96911a1f03280796642e17116
Gedaias <gedaias@geda.dev>
2025-09-10 16:04:54 -0600

    Implement responsive Navbar with mobile menu support

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,51 +1,118 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { MdDashboard } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { CiLogout } from "react-icons/ci";
-import { FiFilePlus } from "react-icons/fi";
+import { FiArrowRight, FiCalendar, FiFilePlus, FiMenu } from "react-icons/fi";
 import { AppointmentModal } from "./AppointmentModal";
 
 export function Navbar() {
+  const [isMobile, setIsMobile] = useState();
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 640);
+    window.addEventListener("resize", handleResize);
+
+    setIsMobile(window.innerWidth <= 640 ? true : false);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-branding">
+        <div className="brand">
+          <span>
+            <FiCalendar />
+          </span>
+          <h3>AppointmentPro</h3>
+        </div>
+      </div>
+      {isMobile ? <MobileMenu /> : <NavbarMenu />}
+    </div>
+  );
+}
+
+const MobileMenu = () => {
   const [activeMenu, setActiveMenu] = useState(false);
-  const [activeModal, setActiveModal] = useState(false);
-  const navigate = useNavigate();
+
+  return (
+    <>
+      <div
+        className="mobile-menu-button"
+        onClick={() => setActiveMenu(!activeMenu)}
+      >
+        <FiMenu />
+      </div>
+      <div
+        className="navbar-mobile-menu"
+        style={{
+          top: `${activeMenu ? "100%" : "-20%"}`,
+          opacity: `${activeMenu ? "1" : "0"}`,
+          pointerEvents: `${activeMenu ? "all" : "none"}`,
+        }}
+      >
+        <NavbarMenu />
+      </div>
+    </>
+  );
+};
+
+const NavbarMenu = () => {
   const { userData, userIsLogged } = useAuth();
+  const [activeModal, setActiveModal] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(false);
+  const navigate = useNavigate();
 
   const getName = () => (userData ? userData.name.split(" ")[0] : "pal");
 
   const toggleModal = () => setActiveModal(!activeModal);
 
   return (
-    <div className="navbar">
-      <div className="navbar-branding">
-        <Link to={"/"}>Brand</Link>
+    <div className="navbar-menu">
+      <div className="navbar-menu-links">
+        <ul>
+          <Link>
+            <li>Features</li>
+          </Link>
+          <Link>
+            <li>Pricing</li>
+          </Link>
+          <Link>
+            <li>View Demo</li>
+          </Link>
+        </ul>
       </div>
-      {userIsLogged ? (
-        <>
-          <div
-            className="user-menu-nav"
-            onClick={() => setActiveMenu(!activeMenu)}
+      <div className="menu-button">
+        {userIsLogged ? (
+          <>
+            <div
+              className="user-menu-nav"
+              onClick={() => setActiveMenu(!activeMenu)}
+            >
+              <p>{`Welcome, ${getName()}`}</p>
+              <IoMdArrowDropdown />
+            </div>
+            <UserMenu activeMenu={activeMenu} toggleModal={toggleModal} />
+            <AppointmentModal
+              activeModal={activeModal}
+              toggleModal={toggleModal}
+            />
+          </>
+        ) : (
+          <button
+            className="login-button-nav"
+            onClick={() => navigate("/login")}
           >
-            <p>{`Welcome, ${getName()}`}</p>
-            <IoMdArrowDropdown />
-          </div>
-          <UserMenu activeMenu={activeMenu} toggleModal={toggleModal} />
-          <AppointmentModal
-            activeModal={activeModal}
-            toggleModal={toggleModal}
-          />
-        </>
-      ) : (
-        <button className="login-button-nav" onClick={() => navigate("/login")}>
-          Login
-        </button>
-      )}
+            Welcome back <FiArrowRight />
+          </button>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 function UserMenu({ activeMenu, toggleModal }) {
   const { logout } = useAuth();
