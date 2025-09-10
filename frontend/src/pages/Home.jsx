4567772584e3e8ddca7df2ff3f203ec6fca4bd7b
Gedaias <gedaias@geda.dev>
2025-09-10 10:06:48 -0600

    Add HeroSection component to Home page with introductory content and styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import "../styles/Home.css";
 import { Navbar } from "../components/Navbar";
+import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
@@ -7,7 +9,29 @@ export function Home() {
       <header>
         <Navbar />
       </header>
-      <section></section>
+      <HeroSection />
     </main>
   );
 }
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <span className="hero-subtitle">Trusted by 1000+ Businesses</span>
+        <h1>
+          Smart Appointment Management for <span>Modern Businesses</span>{" "}
+        </h1>
+        <p>
+          Manage your appointments with ease and and grow your business with our
+          intelligent appointment management platform.
+        </p>
+        <Link to="/signup">
+          <button>
+            Get Started Free <FiArrowRight />
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
