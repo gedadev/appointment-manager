63a22a8fdd5507b7f363619a10e7c2b79ffc2f4c
Gedaias <gedaias@geda.dev>
2025-09-10 22:11:55 -0600

    Add dbName to mongoose connection to specify the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "appointmentsDB" });
     console.log("Connection successful");
   } catch (error) {
     console.error("Connection error:", error);
