6cacd0b444dee842de33b26766dd1f1dd4e41b25
Gedaias <gedaias@geda.dev>
2025-09-29 08:57:40 -0600

    Add customer details toggle and styling in CustomersManager

--- frontend/src/components/Dashboard/CustomersManager.jsx
+++ frontend/src/components/Dashboard/CustomersManager.jsx
@@ -1,5 +1,7 @@
+import { FiChevronsDown, FiChevronsUp } from "react-icons/fi";
 import { useAppointment } from "../../hooks/useAppointment";
 import { months } from "../../utils/main";
+import { useState } from "react";
 
 export function CustomersManager() {
   const { customers } = useAppointment();
@@ -18,6 +20,7 @@ export function CustomersManager() {
 
 const CustomerItem = ({ customer }) => {
   const { formatDate } = useAppointment();
+  const [activeDetails, setActiveDetails] = useState(false);
 
   const getDate = (appointmentDate) => {
     const { month, date, year } = formatDate(appointmentDate);
@@ -29,13 +32,51 @@ const CustomerItem = ({ customer }) => {
     <div className="customer-item">
       <div className="customer-data">
         <div>
-          <h4>Name</h4>
-          <p>{customer.customerName}</p>
+          <h4>{customer.customerName}</h4>
         </div>
         <div>
           <h4>Client since</h4>
           <p>{getDate(customer.createdAt)}</p>
         </div>
+      </div>
+      <div
+        className="see-details"
+        onClick={() => setActiveDetails(!activeDetails)}
+      >
+        {activeDetails ? (
+          <span>
+            Hide customer details <FiChevronsUp />
+          </span>
+        ) : (
+          <span>
+            See customer details <FiChevronsDown />
+          </span>
+        )}
+      </div>
+      <div
+        className="customer-details"
+        style={{
+          opacity: activeDetails ? "1" : "0",
+          transform: activeDetails ? "scaleY(1)" : "scaleY(0)",
+          maxHeight: activeDetails ? "100rem" : "0",
+          marginTop: activeDetails ? "0.5rem" : "0",
+        }}
+      >
+        <form>
+          <div className="short-input">
+            <label>Email:</label>
+            <input type="text" name="email" value={customer.email} />
+          </div>
+          <div className="short-input">
+            <label>Phone:</label>
+            <input type="text" name="phone" value={customer.phone} />
+          </div>
+          <div>
+            <label>Notes:</label>
+            <textarea name="notes">{customer.notes}</textarea>
+          </div>
+          <button>Update</button>
+        </form>
       </div>
     </div>
   );
